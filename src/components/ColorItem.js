ee4191d83873f3f25dac4f4e1afbc8bd965f5b85
jacksonohallo <jacksonohallo@gmail.com>
2022-07-17 13:15:36 +0300

    done

---
+++ src/components/ColorItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ColorList from "./ColorList";
+
+function ColorItem(props) {
+    return <li style={{ color: props.color }}>{props.color}</li>;
+}
+
+
+function ColorList() {
+  const colors = [
+    "firebrick",
+    "rebeccapurple",
+    "salmon",
+    "darkslategray",
+    "hotpink",
+  ];
+
+  const colorElements = colors.map((color) => {
+    return <ColorItem key={color} color={color} />;
+  });
+
+}
